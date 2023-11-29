347afd74646a3411ddf0b05c834f81eee221e4bc
alexisbrazier <103451554+alexisbrazier@users.noreply.github.com>
2023-11-29 15:01:12 -0500

    Update categories.component.js

--- Course2Mod4Solution/src/categories.component.js
+++ Course2Mod4Solution/src/categories.component.js
@@ -1,1 +1,16 @@
-
+angular.module('MenuApp')
+.component('categories', {
+  bindings: { categories: '<' },
+  template: `
+    <h1>categories</h1>
+    
+    <ul>
+      <li ng-repeat="user in $ctrl.categories[0]" ui-sref-active="userselected" >
+      <a href="#!/item?categoryselect={{user}}">{{user}}
+      </a>
+      </li>
+    </ul>
+    
+    <div ui-view></div>
+`,
+})
