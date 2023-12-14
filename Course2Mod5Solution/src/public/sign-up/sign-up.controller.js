b39c48449b5fcd0cdfbfd686b06d1bc846538603
alexisbrazier <103451554+alexisbrazier@users.noreply.github.com>
2023-12-14 13:53:15 -0500

    Update sign-up.controller.js

--- Course2Mod5Solution/src/public/sign-up/sign-up.controller.js
+++ Course2Mod5Solution/src/public/sign-up/sign-up.controller.js
@@ -2,20 +2,66 @@
   "use strict";
   
   angular.module('public')
-  .controller('SignUpController', SignUpController)
-  .service('SignUpService', SignUpService);
+  .controller('SignUpController', SignUpController);
   
-  SignUpController.$inject = ['$scope'];
-  function SignUpController($scope) {
+  SignUpController.$inject = ['$scope','$http'];
+  function SignUpController($scope, $http) {
+
     $scope.userList = [];
     $scope.formSubmitted = "";  
     $scope.showError = false;
     $scope.errorMessage = "";
 
+    function constructUrl(favorite) {
+      var service = this;
+      var baseApi = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+      try{
+        var letters = favorite.match(/[a-zA-Z]+/).join()
+        var numbers = favorite.match(/\d+/);
+  
+        return baseApi.concat(letters.toUpperCase(),"/menu_items/",numbers,".json");
+      } catch (error){
+        $scope.showError = true;
+        return;
+      }
+    }
+
+    function getUrl(url){
+      return $http( {
+        method: 'GET',
+        url: (url)
+      })
+      .catch(error => {
+        $scope.showError = true;
+        return false; // Returns false if an error occurs or the URL does not exist
+      });
+    }
+
+    function testUrl(menuItem) {
+      menuItem.then(response => {
+        try{
+          if (response.data.length>0){
+            console.log("response length", response.data.length)
+            return true;
+          }  
+          else {
+            console.group("error caught looking for response data length > 0 ");
+            return false;
+          }
+        } catch (error) {
+          console.log("error caught looking for response data length");
+          return false;
+        }
+
+      })
+    }
+
 
     $scope.submitForm = function(){
-    
-
+      $scope.userList = []; //clear the previous users
+      $scope.showError = false; //clear previous errors
+      $scope.errorMessage = ""; //clear error message
+      $scope.formSubmitted = ""; //clear submit button
 
       var signUpData = {
         fname: $scope.newuser.fname,
@@ -24,49 +70,27 @@
         phone: $scope.newuser.phone,
         favorite: $scope.newuser.favorite
       };
-      console.log(SignUpService.CheckMenuItem(signUpData.favorite))
-      $scope.showError = SignUpService.checkMenuItem(signUpData.favorite)
-      if ($scope.ShowError) [
-        $scope.errorMessage = "No such menu number exists"
-      ]
+
+      var MenuApi = constructUrl(signUpData.favorite);
+      console.log("MenuApi:", MenuApi);
+      console.log("showerror:", $scope.showError);
+
+      if(!$scope.showError) {
+        var MenuItem = getUrl(MenuApi);
+        console.log(MenuItem);
+      }
+
+      if ($scope.showError) {
+        $scope.errorMessage = "No such menu number exists, try again" ;
+        console.log($scope.errorMessage);
+      } else {
+        $scope.userList.push(signUpData);
+        $scope.formSubmitted = "Your information has been saved";
+      }
 
 
-
-
-      $scope.userList.push(signUpData);
-      
-      $scope.formSubmitted = "Your information has been saved";
-
-      console.log('Sign Up Data:', signUpData);
-      console.log('User List', $scope.userList)
-      // console.log('formsubmitted function:', formSubmitted)
     }
 
   }
 
-  SignUpService.$inject = ['$q', 'categoryShortname']
-  function SignUpService($q, favorite){
-    var service = this;
-    var baseApi = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
-
-    service.checkMenuItem = function(favorite) {
-      if (favorite == 1) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    service.getCategory = function(favorite) {
-      var MenuApi = baseApi.concat(favorite,".json")
-      return $http({
-        method: "GET",
-        url: (MenuApi)
-      }).then(function(result){
-        return result;
-      })
-    console.log(result);
-    }
-  }
-  
   })();
