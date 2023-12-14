f6d47d6438f7d25180936c5aa20164c4cf84e265
alexisbrazier <103451554+alexisbrazier@users.noreply.github.com>
2023-12-14 11:12:37 -0500

    Update my-info.controller.js

--- Course2Mod5Solution/src/public/my-info/my-info.controller.js
+++ Course2Mod5Solution/src/public/my-info/my-info.controller.js
@@ -1,1 +1,25 @@
+    (function () {
+        "use strict";
+        
+        angular.module('public')
+        .controller('MyInfoController', MyInfoController)
+        
+        MyInfoController.$inject = ['$scope'];
+        function MyInfoController($scope) {
+            var userNo = $scope.userList.length;
+            if(userNo>0) {
+                var UserDisplay = true;
+                console.log(UserDisplay);
+            }
+            else {
+                var UserDisplay = false;
+                console.log(UserDisplay);
+            }
+            var user = $scope.userList[userNo];
+            console.log(user);
+          }
+      
+        
 
+        
+        })();
