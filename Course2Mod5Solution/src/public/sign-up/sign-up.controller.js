8ff0861710d3ed32c7ceac0dfa518aa8254519ea
alexisbrazier <103451554+alexisbrazier@users.noreply.github.com>
2023-12-14 11:42:48 -0500

    Update sign-up.controller.js

--- Course2Mod5Solution/src/public/sign-up/sign-up.controller.js
+++ Course2Mod5Solution/src/public/sign-up/sign-up.controller.js
@@ -1,54 +1,72 @@
 (function () {
-    "use strict";
-    
-    angular.module('public')
-    .controller('SignUpController', SignUpController)
-    .service('SignUpService', SignUpService);
-    
-    SignUpController.$inject = ['$scope'];
-    function SignUpController($scope) {
-      $scope.userList = [];
-      $scope.formSubmitted = "";  
+  "use strict";
+  
+  angular.module('public')
+  .controller('SignUpController', SignUpController)
+  .service('SignUpService', SignUpService);
+  
+  SignUpController.$inject = ['$scope'];
+  function SignUpController($scope) {
+    $scope.userList = [];
+    $scope.formSubmitted = "";  
+    $scope.showError = false;
+    $scope.errorMessage = "";
 
-      $scope.submitForm = function(){
+
+    $scope.submitForm = function(){
+    
+
+
+      var signUpData = {
+        fname: $scope.newuser.fname,
+        lname: $scope.newuser.lname,
+        email: $scope.newuser.email,
+        phone: $scope.newuser.phone,
+        favorite: $scope.newuser.favorite
+      };
+      console.log(SignUpService.CheckMenuItem(signUpData.favorite))
+      $scope.showError = SignUpService.checkMenuItem(signUpData.favorite)
+      if ($scope.ShowError) [
+        $scope.errorMessage = "No such menu number exists"
+      ]
+
+
+
+
+      $scope.userList.push(signUpData);
       
+      $scope.formSubmitted = "Your information has been saved";
 
-
-        var signUpData = {
-          fname: $scope.newuser.fname,
-          lname: $scope.newuser.lname,
-          email: $scope.newuser.email,
-          phone: $scope.newuser.phone,
-          favorite: $scope.newuser.favorite
-        };
-
-
-        $scope.userList.push(signUpData);
-        
-        $scope.formSubmitted = "Your information has been saved";
-
-        console.log('Sign Up Data:', signUpData);
-        console.log('User List', $scope.userList)
-        // console.log('formsubmitted function:', formSubmitted)
-      }
-
+      console.log('Sign Up Data:', signUpData);
+      console.log('User List', $scope.userList)
+      // console.log('formsubmitted function:', formSubmitted)
     }
 
-    SignUpService.$inject = ['$q', 'categoryShortname']
-    function SignUpService($q, favorite){
-      var service = this;
-      var baseApi = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+  }
 
-      service.getCategory = function(favorite) {
-        var MenuApi = baseApi.concat(favorite,".json")
-        return $http({
-          method: "GET",
-          url: (MenuApi)
-        }).then(function(result){
-          return result;
-        })
-      console.log(result);
+  SignUpService.$inject = ['$q', 'categoryShortname']
+  function SignUpService($q, favorite){
+    var service = this;
+    var baseApi = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+
+    service.checkMenuItem = function(favorite) {
+      if (favorite == 1) {
+        return true;
+      }
+      else {
+        return false;
       }
     }
-    
-    })();
+    service.getCategory = function(favorite) {
+      var MenuApi = baseApi.concat(favorite,".json")
+      return $http({
+        method: "GET",
+        url: (MenuApi)
+      }).then(function(result){
+        return result;
+      })
+    console.log(result);
+    }
+  }
+  
+  })();
